43e313c786c0ffe6b33bc20bfbf00b95f17f58ff
JEANJACQUOT Thomas <thomas.jeanjacquot@telecomnancy.eu>
2022-11-22 10:15:21 +0000

    Suppression des fichiers inutiles

--- code/jardinmanager.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-from flask import request 
-from flask import render_template  
-app=Flask(__name__)
-
-@app.route('/forum',methods=['GET','POST'])
-def forum():     
-    if request.method=='GET':
-        return render_template('forum.html')
