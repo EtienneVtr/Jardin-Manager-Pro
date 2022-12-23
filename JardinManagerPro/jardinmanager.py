e1edff15ae2aa8d78c426fda4f82555690196182
JEANJACQUOT Thomas <thomas.jeanjacquot@telecomnancy.eu>
2022-12-23 11:19:25 +0100

    Correction du main app.config

--- JardinManagerPro/jardinmanager.py
+++ JardinManagerPro/jardinmanager.py
@@ -8,12 +8,14 @@ import sqlite3
 
 #importation des fonctions créée:
 from fonctions import *
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+
 
 # flask app creation
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 #def des routes:
