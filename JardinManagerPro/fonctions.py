c5d3d7a52a652fe577fb06819149ccc22f9cc7f9
Etienne <etienne.vatry@telecomnancy.eu>
2022-12-26 10:25:02 +0100

    merge toujours a la main

--- JardinManagerPro/fonctions.py
+++ JardinManagerPro/fonctions.py
@@ -97,6 +97,57 @@ def fct_inscritpion(pseudo, mail, mdp, ville):
     else :
         return render_template("error_profil.html", message = "Pseudo ou mail déjà pris !")
     
+# fonction pour changer de pseudo,...
+def maj_db(pseudo, nouvelle_donnee, donnee_a_changer):
+    if donnee_a_changer == "Pseudo":
+        query = """UPDATE profils SET Pseudo = ? WHERE Pseudo LIKE ?;"""
+        args = [nouvelle_donnee,pseudo]
+        db, cursor = connectDatabase()
+        cursor.execute(query,args)
+        db.commit()
+        db.close()
+        
+        return redirect("/profil")
+    elif donnee_a_changer == "Mail":
+        query = """UPDATE profils SET Mail = ? WHERE Pseudo LIKE ?;"""
+        args = [nouvelle_donnee,pseudo]
+        db, cursor = connectDatabase()
+        cursor.execute(query,args)
+        db.commit()
+        db.close()
+        
+        return redirect("/profil")
+    elif donnee_a_changer == "Mdp":
+        query = """UPDATE profils SET Mdp = ? WHERE Pseudo LIKE ?;"""
+        args = [nouvelle_donnee,pseudo]
+        db, cursor = connectDatabase()
+        cursor.execute(query,args)
+        db.commit()
+        db.close()
+        
+        return redirect("/profil")
+    else :
+        query = """UPDATE profils SET Ville = ? WHERE Pseudo LIKE ?;"""
+        args = [nouvelle_donnee,pseudo]
+        db, cursor = connectDatabase()
+        cursor.execute(query,args)
+        db.commit()
+        db.close()
+        
+        return redirect("/profil")
+    return redirect("/profil")
+    
+# fonction gérant affichage profil public
+def fct_profil_public(pseudo):
+    query = """SELECT Ville FROM profils WHERE Pseudo LIKE (?)"""
+    args = [pseudo]
+    db, cursor = connectDatabase()
+    cursor.execute(query, args)
+    data = cursor.fetchall()
+    db.close()
+    
+    return render_template("profil_public.html", items = data, pseudo = pseudo)
+    
 #base de donnéé forum
 
 def connectdbforum():
